35cd6cc0862339110e75f74b6326fdb654d67862
r.fairlie <git@chaoticgoose.com>
2024-12-30 22:29:48 +1100

    feat: RSS feed

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,8 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 module.exports = eleventyConfig => {
+    eleventyConfig.addPlugin(pluginRss);
 
-	eleventyConfig.addShortcode('excerpt', post => extractExcerpt(post));
-
+	eleventyConfig.addShortcode('excerpt', post => extractExcerpt(post)); // for {{ exerpt <post> }}
 	function extractExcerpt(post) {
 		if(!post.templateContent) return '';
 		if(post.templateContent.indexOf('</p>') > 0) {
